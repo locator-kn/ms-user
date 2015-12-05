1ebcc845c7a97d5d971593a470d75b038c575f30
Timo Weiß <info@timo-weiss.com>
2015-12-05 16:44:44 +0100

    update script

--- generate_docs.sh
+++ generate_docs.sh
@@ -3,8 +3,17 @@
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 if [ $current_branch != 'master' ];
 then
-    return
+    echo "Im doing nothing, you are not on master branch"
+    exit 1
 fi
+
+if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]];
+then
+    echo "Im doing nothing, you got something dirty"
+    exit 1
+fi
+
+
 rm -rf docs/*
 npm run documentation
 
