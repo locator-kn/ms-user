9b05e25447bb5776d53798b318901efe12f6da72
Timo Weiß <info@timo-weiss.com>
2015-12-20 03:17:16 +0100

    add dummy impl

--- test/stubs/database.stub.js
+++ test/stubs/database.stub.js
@@ -2,8 +2,14 @@
 
 const fns = {};
 
+const userFixtures = require('../fixtures/user');
+
 fns.getUser = mail => {
     //return database.collection('users').findOne({"mail": mail});
+    if (mail === userFixtures.userLoginPass.data.mail) {
+        return Promise.resolve(userFixtures.userRegisterPass.data);
+    }
+    return Promise.resolve(null);
 };
 
 fns.createUser = user => {
