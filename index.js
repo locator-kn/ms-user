0534f88de4fea3485dbfb6650b65d25d3a9dd1ee
Timo Weiß <info@timo-weiss.com>
2016-01-21 18:16:12 +0100

    add missing util, fix jshint error

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const path = require('path');
 const pwd = path.join(__dirname, '..', '/.env');
 require('dotenv').config({path: pwd});
 
+
+const util = require('ms-utilities');
 const seneca = require('seneca')();
 const user = require('./lib/user');
 const database = require('./lib/database');
 
 
 // select desired transport method
-const transportMethod = process.env['SENECA_TRANSPORT_METHOD'] || 'rabbitmq';
+//const transportMethod = process.env['SENECA_TRANSPORT_METHOD'] || 'rabbitmq';
 const patternPin = 'role:user';
 
 // init database and then seneca and expose functions
