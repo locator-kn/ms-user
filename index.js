ac5d8641a809340ade7eae8c007fb67f1beb8e9d
Silvia Goetti <sigoetti@htwg-konstanz.de>
2016-02-28 20:53:37 +0100

    prepared for facebook login

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ database.connect()
             .add(patternPin + ',cmd:getfollowing', user.getFollowing)
             .add(patternPin + ',cmd:getUserById', user.getUserById)
             .add(patternPin + ',cmd:count,entity:follower,by:userId', user.getFollowersCountByUserId)
+            .add(patternPin + ',cmd:fbLogin', user.fbLogin)
             //.act({
             //    role: 'user',
             //    cmd: 'follow',
