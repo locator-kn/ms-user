5710fab677f18537e510666d8216a4b9f3172fe2
Steffen Gorenflo <SteffenGorenflo@gmail.com>
2016-04-16 14:43:37 +0200

    add new service and some format style and logging

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const pwd = path.join(__dirname, '..', '/.env');
 require('dotenv').config({path: pwd});
 
 
-//const util = require('ms-utilities');
+const log = require('ms-utilities').logger;
 const seneca = require('seneca')();
+
 const user = require('./lib/user');
 const device = require('./lib/device');
 const database = require('./lib/database');
@@ -22,12 +23,13 @@ database.connect()
         seneca
         //.use(transportMethod + '-transport')
 
-            //.client({type: 'tcp', port: 7010, host: 'localhost', pin: 'role:reporter'})
+        //.client({type: 'tcp', port: 7010, host: 'localhost', pin: 'role:reporter'})
 
             .add(patternPin + ',cmd:register,entity:device', device.registerDevice)
             .add(patternPin + ',cmd:unregister,entity:device', device.unregisterDevice)
             .add(patternPin + ',cmd:register,entity:user', user.register)
-            .add(patternPin + ',cmd:get,entity:pushToken',device.getPushToken)
+            .add(patternPin + ',cmd:get,entity:pushToken', device.getPushToken)
+            .add(patternPin + ',cmd:get,entity:device', device.getDevice)
 
             .add(patternPin + ',cmd:add,entity:image', user.addImageToUser)
 
@@ -42,19 +44,23 @@ database.connect()
             .add(patternPin + ',cmd:getUser,by:mail', user.getUserByMail)
             .add(patternPin + ',cmd:count,entity:follower,by:userId', user.getFollowersCountByUserId)
             .add(patternPin + ',cmd:fbLogin', user.fbLogin)
-            .add(patternPin + ',cmd:unfollow',user.unFollow)
+            .add(patternPin + ',cmd:unfollow', user.unFollow)
             /*.act({
-                role: 'user',
-                cmd: 'get',
-                entity: 'pushToken',
-                data: {
-                    user_ids: ['569e4a83a6e5bb503b838306', '56df59dc8c151b9053a5f73f']
-                }
-            }, (err, data) => {
-                console.log('follow resp:', err || data);
-            })*/
+             role: 'user',
+             cmd: 'get',
+             entity: 'pushToken',
+             data: {
+             user_ids: ['569e4a83a6e5bb503b838306', '56df59dc8c151b9053a5f73f']
+             }
+             }, (err, data) => {
+             console.log('follow resp:', err || data);
+             })*/
             //.listen({type: transportMethod, pin: patternPin});
             //.listen({type: 'tcp', port: 7002, pin: patternPin});
-            .use('mesh',{auto:true, pin:patternPin});
-            //.wrap(patternPin, util.reporter.report);
+            .use('mesh', {auto: true, pin: patternPin});
+        //.wrap(patternPin, util.reporter.report);
+    })
+    .catch(err => {
+        log.fatal('Unable to start ms-user', err);
+        throw err;
     });
